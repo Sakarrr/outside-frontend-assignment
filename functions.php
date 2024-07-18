583450c0dab816287189872639a13494b47e8cf4
Sakarr <ctha.sakar@gmail.com>
2024-07-18 22:39:05 +0545

    Remove - Block init from theme

--- functions.php
+++ functions.php
@@ -16,25 +16,3 @@ function enqueue_splide_js() {
 	wp_enqueue_script( 'splide-js', 'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js', array(), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_splide_js' );
-
-function outside_block_enqueues() {
-	wp_enqueue_script( 'outside-block', get_template_directory_uri() . '/assets/js/block.bundle.js', array( 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components' ) );
-
-	//    wp_enqueue_style('outside-block-editor', get_template_directory_uri().'/assets/css/editor.css', array('wp-edit-blocks'));
-	//
-	//    wp_enqueue_style('outside-block-frontend', get_template_directory_uri().'/assets/css/outside-frontend.css');
-}
-add_action( 'enqueue_block_editor_assets', 'outside_block_enqueues' );
-add_action( 'wp_enqueue_scripts', 'outside_block_enqueues' );
-
-function register_outside_block() {
-	register_block_type(
-		'outside-block',
-		array(
-			'editor_script' => 'outside-block',
-			'editor_style'  => 'outside-block-editor',
-			'style'         => 'outside-block-frontend',
-		)
-	);
-}
-add_action( 'init', 'register_outside_block' );
